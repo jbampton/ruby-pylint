104056cb4aec1c4c5173ad402d149115d92fdea7
John Bampton <jbampton@users.noreply.github.com>
2018-01-11 18:00:04 +1000

    Add code.

---
+++ pylint.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+# Ruby command line script that runs the Pylint python code linting tool.
+# https://www.pylint.org/ against a whole directory recursively
+
+#
+def lint(src)
+  Dir.glob("#{src}/**/*.py").map do |filename|
+    puts `pylint "#{filename}"` unless filename.include? 'venv'
+  end
+end
+
+if ARGV[0].nil?
+  STDOUT.puts 'Please provide the directory - /path/to/dir - to run pylint ' \
+              'against or use . for the current directory.'
+else
+  lint(ARGV[0])
+end
